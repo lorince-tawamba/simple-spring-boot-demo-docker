7fbcddc4c57756ce75c6bd172a5d36145b75e51f
LorinceTawamba <lorince.tawamba@gmail.com>
2022-02-07 03:37:50 +0100

    Initial commit

---
+++ src/main/java/tech/satic/docker/SimpleSpringBootDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package tech.satic.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootDemoDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootDemoDockerApplication.class, args);
+    }
+
+}
